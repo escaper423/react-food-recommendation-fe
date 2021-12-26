da7e107482e436684b22942d07a9b8ce507c7818
escaper423 <escaper423@gmail.com>
2021-12-26 22:44:22 +0900

    some constraints added

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -26,7 +26,7 @@ const SearchResult = () => {
             }
         }).then(res => {
             console.log(res.data)
-            setQueryData(res.data[0])
+            setQueryData(res.data[0].slice(0,10))
             setSumAmount(res.data[1])
         })
     }, [])
@@ -64,7 +64,7 @@ const SearchResult = () => {
                     <p>추천 우선순위는 현재 가장 많은 표본을 가진 데이터 수 입니다. &nbsp;추후 다른 기준을 적용할 예정입니다.</p>
                     <p>가장 최근에 먹은 음식</p>
                     <div style={{ width: '192px', height: '192px', margin: 'auto', marginBottom: '30px' }}>
-                        <img style={{ borderRadius: '50%', width: '100%', height: '100%' }}
+                        <img style={{ borderRadius: '10px', width: '100%', height: '100%' }}
                             src={noImage} alt="Food Image" loading="lazy" />
                     </div>
                     <h2>추천 음식들</h2>
